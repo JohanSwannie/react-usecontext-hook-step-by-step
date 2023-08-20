ac03f15d7aacee450c588d5386f7552de3c8bb43
jswan <jswannieman@gmail.com>
2023-08-20 12:14:32 +1200

    Update DisplayThemeF Javascript Component

--- src/DisplayThemeF.js
+++ src/DisplayThemeF.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTheme, useToggle } from "./ThemeProvider";
+import { useTheme, useToggle } from "./ThemeContext";
 
 export default function DisplayThemeF() {
   const darkTheme = useTheme();
