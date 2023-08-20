5398655a19e0922e3a8f728f7d93c2d706a941e4
jswan <jswannieman@gmail.com>
2023-08-20 12:14:12 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DisplayThemeF from "./DisplayThemeF";
-import { ThemeProvider } from "./ThemeProvider";
+import { ThemeProvider } from "./ThemeContext";
 import "./App.css";
 
 function App() {
